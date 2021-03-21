b968cc51093aaf015c816e951208b464692744f6
Jaryd Hermann <hermannjaryd@gmail.com>
2021-03-21 13:19:04 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -6,8 +6,28 @@ module Players
 # Even though the method accepts a board argument, it does not need to use it.
 
     def move(board)
-      input = gets.strip
+
+      case
+      when board.cells[4] == " "
+        "5"
+      when board.cells[0] == " "
+        "1"
+      when board.cells[2] == " "
+        "3"
+      when board.cells[6] == " "
+        "7"
+      when board.cells[8] == " "
+        "9"
+      when board.cells[1] == " "
+        "2"
+      when board.cells[3] == " "
+        "4"
+      when board.cells[5] == " "
+        "6"
+      when board.cells[7] == " "
+        "8"
+      end
     end
-    
+
   end #end class
 end #end module
